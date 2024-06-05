659221ca004aa70e368d2b99f72ddbaa55426a66
Saifullah <78204201+Saifullah00@users.noreply.github.com>
2024-06-05 14:39:35 +0000

    Inital commit

---
+++ memegenerator/src/components/Header.jsx
@@ -0,0 +1,14 @@
+
+export default function Header(){
+    return (
+    <>
+        <header className="heading">
+            <img width="48" height="48" src="https://img.icons8.com/color/48/my-name-is-jeff.png" alt="my-name-is-jeff"/>
+            
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--owner">Made by Saifullah</h4>
+        </header>
+    </>
+    )
+
+}
---
+++ memegenerator/src/components/Meme.jsx
@@ -0,0 +1,25 @@
+
+export default function Meme(){
+    return(
+        <>
+            <main>
+                <form action="#" className="form">
+                
+                    <input 
+                        type="text" 
+                        placeholder="Top text"
+                        className="form--input" />
+                    
+                    <input 
+                        type="text" 
+                        placeholder="Bottom text"
+                        className="form--input" />
+                    
+                    <button className="form--button">
+                        Get a new meme image 🖼
+                    </button>
+                </form>
+            </main>
+        </>
+    )
+}
